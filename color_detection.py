ac291fd6fba6c368f640763ca67d4525a6db370c
te-yd <rithicks0629@gmail.com>
2021-11-21 10:11:33 +0530

    Faster, Invalid Logic

--- color_detection.py
+++ color_detection.py
@@ -2,29 +2,28 @@ from pprint import pprint
 import cv2
 import numpy as np
 import pandas as pd
-from multiprocessing import Pool
+from multiprocessing import Process, Array
 
 df = pd.read_csv(
 		'colors.csv',
 		names = ['color', 'color_name', 'hex', 'R', 'G', 'B'],
 		header=None)
-results = np.zeros(len(df), dtype=int)
+results = np.zeros(len(df))
 df_nodes = df.to_numpy()
 nodes = df_nodes[..., (3, 4, 5)]
 
 img = cv2.imread('pic1.jpg')
-img = cv2.resize(img, (800,600))
-img = img.reshape((800 * 600, 3))
+img = img.reshape((img.shape[0] * img.shape[1], 3))
 
 def get_color_name_optimized_v5(RGB):
-	global results
 	index = np.sum(np.power(nodes - RGB, 2), axis = 1)
 	c = np.argmin(index)
-	results[c] += 1
 	return c
 
-def get_color_statistics_v2(img):
-	with Pool(5) as p:
-		c = p.map(get_color_name_optimized_v5, img[0:16000])
-	pprint(results[results != 0])
-	
+def get_color_final(img):
+	results[get_color_name_optimized_v5([0, 0, 0])] = np.count_nonzero(img == [0, 0, 0])
+	img = img[img != [0, 0, 0]]
+	while len(img) != 0:
+		now = img[0]
+		results[get_color_name_optimized_v5(now)] += np.count_nonzero(img == now)
+		img = img[img != now]
