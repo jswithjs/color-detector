3137c2df9218adac4d881c24f5eeb2fc0557cdbb
te-yd <rithicks0629@gmail.com>
2022-01-13 07:24:05 +0530

    Binary Form Data

--- main.py
+++ main.py
@@ -18,29 +18,6 @@ names      =  df_nodes[..., 1]
 hex_names  =  df_nodes[..., 2]
 name_hex   =  df_nodes[..., 2].tolist()
 
-def hex_nearest_match(h):
-    if h in hex_names:
-        return h
-    
-    h_I = int(h[1: ], 16)
-    
-    lb = hex_names[hex_names < h]
-    ub = hex_names[hex_names > h]
-    
-    if lb.shape[0]:
-        res =  ub[0]
-    elif ub.shape[0]:
-        res =  lb[-1]
-    else:    
-        ub_I = int(ub[1: ], 16)
-        lb_I = int(lb[1: ], 16)
-        if h_I - lb_I < ub_I - h_I:
-            res =  lb
-        else:
-            res = ub
-    
-    return names[name_hex.index(res)]
-
 def rgb_to_hex(rgb_color):
     hex_color = "#"
     for i in rgb_color:
@@ -49,8 +26,7 @@ def rgb_to_hex(rgb_color):
     return hex_color
 
 async def prep_image(raw_img):
-    img = b64decode(raw_img)
-    img = np.frombuffer(img, dtype = np.uint8)
+    img = np.frombuffer(raw_img, dtype = np.uint8)
     img = cv2.imdecode(img, flags=cv2.IMREAD_COLOR)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     modified_img = cv2.resize(img, (900, 600), interpolation = cv2.INTER_AREA)
@@ -67,11 +43,10 @@ async def color_analysis(img = Form(...), n = Form(...)):
     counts = Counter(color_labels_l)
     ordered_colors = [center_colors[i] for i in counts.keys()]
     hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
-    hex_color_names = list(map(hex_nearest_match, hex_colors))
 
     ret = {}
     for i in range(0, n):
-        c = hex_color_names[i]
+        c = hex_colors[i]
         if i in ret:
             ret[c] += round((counts[i] * 100) / img.shape[0], 2)
         else:
