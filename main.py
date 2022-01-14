075e98252a6b6c4f6a622b61bd7905b7011075e4
te-yd <rithicks0629@gmail.com>
2022-01-15 04:13:55 +0530

    Release v1.21

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from fastapi import FastAPI, Form, File
-from collections import Counter
+from PIL import Image
 import numpy as np
-from sklearn.cluster import KMeans
 import cv2
-
-app = FastAPI()
+from lib import get_palette
+from fastapi import FastAPI, File, Form
 
 def rgb_to_hex(rgb_color):
     hex_color = "#"
@@ -13,35 +11,37 @@ def rgb_to_hex(rgb_color):
         hex_color += ("{:02x}".format(i))
     return hex_color
 
-async def prep_image(raw_img):
-    img = np.frombuffer(raw_img, dtype = np.uint8)
+async def process(image_buffer, n):
+    img = np.frombuffer(image_buffer, dtype = np.uint8)
     img = cv2.imdecode(img, flags=cv2.IMREAD_COLOR)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    modified_img = cv2.resize(img, (900, 600), interpolation = cv2.INTER_AREA)
-    modified_img = modified_img.reshape(modified_img.shape[0]*modified_img.shape[1], 3)
-    return modified_img
 
-@app.post("/")
-async def color_analysis(img: bytes = File(...), n: int = Form(...)):
-    img = await prep_image(img)
-    clf = KMeans(n_clusters = n)
-    color_labels_l = clf.fit_predict(img)
-    center_colors  = clf.cluster_centers_
-    counts = Counter(color_labels_l)
-    ordered_colors = [center_colors[i] for i in counts.keys()]
-    hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
-
+    IMAGE  = Image.fromarray(img)
+    IMAGE  = IMAGE.resize((1280, 720))
+    _, p_map = get_palette(IMAGE, n, 10)
+    
     ret = {}
-    for i in range(0, n):
-        c = hex_colors[i]
-        if i in ret:
-            ret[c] += round((counts[i] * 100) / img.shape[0], 2)
-        else:
-            ret[c] = round((counts[i] * 100) / img.shape[0], 2)
-    return {
-        "status": 1,
-        "result": ret
-    }
+    for i in p_map.vboxes.contents:
+        color_name = rgb_to_hex(i['color'])
+        ret[color_name] = i['vbox'].count
+    
+    FINAL = sum(ret.values())
+    for i in ret:
+        ret[i] = round((ret[i] / FINAL) * 100, 2)
+    return ret
+
+app = FastAPI()
+
+@app.post('/')
+async def home(img: bytes = File(...), n: int = Form(...)):
+    try:
+        c = await process(img, n)
+        return {
+            "Status": 1
+            , "data": c
+        }
+    except:
+        return {"Error": "Server Error"}
 
 @app.get("/")
 async def home():
---
+++ main_old.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI, Form, File
+from collections import Counter
+import numpy as np
+from sklearn.cluster import KMeans
+import cv2
+
+app = FastAPI()
+
+def rgb_to_hex(rgb_color):
+    hex_color = "#"
+    for i in rgb_color:
+        i = int(i)
+        hex_color += ("{:02x}".format(i))
+    return hex_color
+
+async def prep_image(raw_img):
+    img = np.frombuffer(raw_img, dtype = np.uint8)
+    img = cv2.imdecode(img, flags=cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    modified_img = cv2.resize(img, (900, 600), interpolation = cv2.INTER_AREA)
+    modified_img = modified_img.reshape(modified_img.shape[0]*modified_img.shape[1], 3)
+    return modified_img
+
+@app.post("/")
+async def color_analysis(img: bytes = File(...), n: int = Form(...)):
+    img = await prep_image(img)
+    clf = KMeans(n_clusters = n)
+    color_labels_l = clf.fit_predict(img)
+    center_colors  = clf.cluster_centers_
+    counts = Counter(color_labels_l)
+    ordered_colors = [center_colors[i] for i in counts.keys()]
+    hex_colors = [rgb_to_hex(ordered_colors[i]) for i in counts.keys()]
+
+    ret = {}
+    for i in range(0, n):
+        c = hex_colors[i]
+        if i in ret:
+            ret[c] += round((counts[i] * 100) / img.shape[0], 2)
+        else:
+            ret[c] = round((counts[i] * 100) / img.shape[0], 2)
+    return {
+        "status": 1,
+        "result": ret
+    }
+
+@app.get("/")
+async def home():
+    return { "server": "up and Running!" }
